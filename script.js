8d0cbc8c020f724d8d2b8242369820fa4684f0a5
シミズ <kurumishimizu24@gmail.com>
2024-12-06 22:41:33 +0900

    a

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
             var thisWidth = $this.outerWidth(true); // 外側のマージンも含めた幅を取得
 
             // 合計幅が400pxを超える場合
-            if (totalWidth + thisWidth > $(window).width() * 0.9) {
+            if (totalWidth + thisWidth > $(window).width() * 0.85) {
                 // 新しいdiv要素をbodyに追加
                 $('body').append($newDiv);
                 // 合計幅をリセット
@@ -123,252 +123,159 @@ $(document).ready(function () {
 
             };
 
-            // if (count === 1) {
-            //     // カウントが1になったときに行う処理
-            //     $('.paragraph--2').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 2) {
-            //     $('.paragraph--3').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 3) {
-            //     $('.paragraph--4').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 4) {
-            //     $('.paragraph--5').css({
-            //         'opacity': "1",
-            //     });
-            //     $('.bg-eye').css({
-            //         'opacity': "0",
-            //     });
+            if (count === 4) {
+                $('.paragraph--5').css({
+                    'opacity': "1",
+                });
+                $('.bg-eye').css({
+                    'opacity': "0",
+                });
 
-            // } else if (count === 5) {
-            //     $('.paragraph--6').css({
-            //         'opacity': "1",
-            //     });
-            //     $('.bg-eye').css({
-            //         'opacity': "0.1",
-            //     });
-            // } else if (count === 6) {
-            //     $('.paragraph--7').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 7) {
-            //     $('.paragraph--8').css({
-            //         'opacity': "1",
-            //     });
-            // }
-            // else if (count === 8) {
-            //     $('.paragraph--9').css({
-            //         'opacity': "1",
-            //     });
-            //     $('.bar').css({
-            //         'background-color': "#000",
-            //     })
-            //     $('.body').css({
-            //         'background-color': "#000",
-            //     });
-            //     $('.paragraph__wrapper').css({
-            //         'color': "#F9EFE1",
-            //     });
-            //     $('.help__text').css({
-            //         'color': "#F9EFE1",
-            //     });
-            //     $('.line--top').css({
-            //         'background-color': "#F9EFE1",
-            //     });
-            //     $('.line--bottom').css({
-            //         'background-color': "#F9EFE1",
-            //     });
-            //     $('.line--right').css({
-            //         'background-color': "#F9EFE1",
-            //     }); $('.line--left').css({
-            //         'background-color': "#F9EFE1",
-            //     });
-            //     $('.cls-1').css({
-            //         'fill': "#F9EFE1",
-            //     });
-            //     $('.bg-eye').css({
-            //         'opacity': "0",
-            //     });
-            //     $('.st0').css({
-            //         'fill': "#F9EFE1",
-            //     });
+            } else if (count === 5) {
+                $('.paragraph--6').css({
+                    'opacity': "1",
+                });
+                $('.bg-eye').css({
+                    'opacity': "0.1",
+                });
+            } else if (count === 8) {
+                $('.paragraph--9').css({
+                    'opacity': "1",
+                });
 
-            // }
-            // else if (count === 9) {
-            //     $('#paper')[0].play();
+                $('.body').css({
+                    'background-color': "#000",
+                });
+                $('.paragraph__wrapper').css({
+                    'color': "#F9EFE1",
+                });
+                $('.help__text').css({
+                    'color': "#F9EFE1",
+                });
+                $('.line--top').css({
+                    'background-color': "#F9EFE1",
+                });
+                $('.line--bottom').css({
+                    'background-color': "#F9EFE1",
+                });
+                $('.line--right').css({
+                    'background-color': "#F9EFE1",
+                }); $('.line--left').css({
+                    'background-color': "#F9EFE1",
+                });
+                $('.cls-1').css({
+                    'fill': "#F9EFE1",
+                });
+                $('.bg-eye').css({
+                    'opacity': "0",
+                });
+                $('.st0').css({
+                    'fill': "#F9EFE1",
+                });
 
-            //     $('.paragraph--10').css({
-            //         'opacity': "1",
-            //     });
-            //     $('.bar').css({
-            //         'background-color': "#F9EFE1",
-            //     })
+            }
+            else if (count === 9) {
+                $('#paper')[0].play();
 
-            //     $('.body').css({
-            //         'background-color': "#F9EFE1",
-            //     });
-            //     $('.paragraph__wrapper').css({
-            //         'color': "#0B1632",
-            //     });
-            //     $('.help__text').css({
-            //         'color': "#0B1632",
-            //     });
-            //     $('.line--top').css({
-            //         'background-color': "#0B1632",
-            //     });
-            //     $('.line--bottom').css({
-            //         'background-color': "#0B1632",
-            //     });
-            //     $('.line--right').css({
-            //         'background-color': "#0B1632",
-            //     }); $('.line--left').css({
-            //         'background-color': "#0B1632",
-            //     });
-            //     $('.cls-1').css({
-            //         'fill': "#0B1632",
-            //     });
-            //     $('.bg-eye').css({
-            //         'opacity': "0.1",
-            //     });
-            //     $('.st0').css({
-            //         'fill': "#0B1632",
-            //     });
+                $('.paragraph--10').css({
+                    'opacity': "1",
+                });
 
-            // }
-            // else if (count === 10) {
-            //     $('.paragraph--11').css({
-            //         'opacity': "1",
-            //     });
-            // }
-            // else if (count === 11) {
-            //     $('.paragraph--12').css({
-            //         'opacity': "1",
-            //     });
 
-            //     $('.bar').css({
-            //         'animation': "tikatika .3s step-end forwards"
-            //     })
+                $('.body').css({
+                    'background-color': "#F9EFE1",
+                });
+                $('.paragraph__wrapper').css({
+                    'color': "#0B1632",
+                });
+                $('.help__text').css({
+                    'color': "#0B1632",
+                });
+                $('.line--top').css({
+                    'background-color': "#0B1632",
+                });
+                $('.line--bottom').css({
+                    'background-color': "#0B1632",
+                });
+                $('.line--right').css({
+                    'background-color': "#0B1632",
+                }); $('.line--left').css({
+                    'background-color': "#0B1632",
+                });
+                $('.cls-1').css({
+                    'fill': "#0B1632",
+                });
+                $('.bg-eye').css({
+                    'opacity': "0.1",
+                });
+                $('.st0').css({
+                    'fill': "#0B1632",
+                });
 
-            //     $('.body').css({
-            //         'animation': "tikatika .3s step-end forwards"
-            //     });
-            //     $('.paragraph__wrapper').css({
-            //         'color': "#F9EFE1",
-            //     });
-            //     $('.help__text').css({
-            //         'color': "#F9EFE1",
-            //     });
-            //     $('.line--top').css({
-            //         'background-color': "#F9EFE1",
-            //     });
-            //     $('.line--bottom').css({
-            //         'background-color': "#F9EFE1",
-            //     });
-            //     $('.line--right').css({
-            //         'background-color': "#F9EFE1",
-            //     }); $('.line--left').css({
-            //         'background-color': "#F9EFE1",
-            //     });
-            //     $('.cls-1').css({
-            //         'fill': "#F9EFE1",
-            //     });
-            //     $('.bg-eye').css({
-            //         'fill': "#F9EFE1",
-            //     });
-            //     $('.st0').css({
-            //         'fill': "#F9EFE1",
-            //     });
+            }
+            else if (count === 11) {
+                $('.paragraph--12').css({
+                    'opacity': "1",
+                });
 
-            // }
-            // else if (count === 12) {
-            //     $('.paragraph--13').css({
-            //         'opacity': "1",
-            //     });
-            // }
-            // else if (count === 13) {
-            //     $('.paragraph--14').css({
-            //         'opacity': "1",
-            //     });
-            // }
-            // else if (count === 14) {
-            //     $('.paragraph--15').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 15) {
-            //     $('.paragraph--16').css({
-            //         'opacity': "1",
-            //     });
-            //     $('.bg-eye').css({
-            //         'opacity': "0",
-            //     });
-            // } else if (count === 16) {
-            //     $('.paragraph--17').css({
-            //         'opacity': "1",
-            //     });
-            //     $('.bg-eye').css({
-            //         'opacity': "0.1",
-            //     });
 
-            // } else if (count === 17) {
-            //     $('.paragraph--18').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 18) {
-            //     $('.paragraph--19').css({
-            //         'opacity': "1",
-            //     });
-            //     $('#clock')[0].play();
-            // } else if (count === 19) {
-            //     $('.paragraph--20').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 20) {
-            //     $('.paragraph--21').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 21) {
-            //     $('.paragraph--22').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 22) {
-            //     $('.paragraph--23').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 23) {
-            //     $('.paragraph--24').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 24) {
-            //     $('.paragraph--25').css({
-            //         'opacity': "1",
-            //     });
 
-            // } else if (count === 25) {
-            //     $('#bgm')[0].play();
-            //     $('.paragraph--26').css({
-            //         'opacity': "1",
-            //     });
-            // } else if (count === 26) {
-            //     $('.paragraph--27').css({
-            //         'opacity': "1",
-            //     });
+                $('.body').css({
+                    'animation': "tikatika .3s step-end forwards"
+                });
+                $('.paragraph__wrapper').css({
+                    'color': "#F9EFE1",
+                });
+                $('.help__text').css({
+                    'color': "#F9EFE1",
+                });
+                $('.line--top').css({
+                    'background-color': "#F9EFE1",
+                });
+                $('.line--bottom').css({
+                    'background-color': "#F9EFE1",
+                });
+                $('.line--right').css({
+                    'background-color': "#F9EFE1",
+                }); $('.line--left').css({
+                    'background-color': "#F9EFE1",
+                });
+                $('.cls-1').css({
+                    'fill': "#F9EFE1",
+                });
+                $('.bg-eye').css({
+                    'fill': "#F9EFE1",
+                });
+                $('.st0').css({
+                    'fill': "#F9EFE1",
+                });
 
-            // } else if (count === 27) {
-            //     $('.paragraph--28').css({
-            //         'opacity': "1",
-            //     });
+            } else if (count === 15) {
+                $('.paragraph--16').css({
+                    'opacity': "1",
+                });
+                $('.bg-eye').css({
+                    'opacity': "0",
+                });
+            } else if (count === 16) {
+                $('.paragraph--17').css({
+                    'opacity': "1",
+                });
+                $('.bg-eye').css({
+                    'opacity': "0.1",
+                });
 
-            // }
-            // else if (count === 28) {
-            //     $('.paragraph--29').css({
-            //         'opacity': "1",
-            //     });
-
-            // }
+            } else if (count === 18) {
+                $('.paragraph--19').css({
+                    'opacity': "1",
+                });
+                $('#clock')[0].play();
+            } else if (count === 25) {
+                $('#bgm')[0].play();
+                $('.paragraph--26').css({
+                    'opacity': "1",
+                });
+            };
 
         }
 
