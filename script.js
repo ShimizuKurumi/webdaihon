1b35f6424cf3a0004876296d1cfef981baeaec52
シミズ <kurumishimizu24@gmail.com>
2024-12-30 21:20:21 +0900

    a

--- script.js
+++ script.js
@@ -711,89 +711,89 @@ function clickBtn2() {
 //     });
 // });
 
-//もや
-$(function () {
-  var isDown = true;
-  var isLeft = true;
+// //もや
+// $(function () {
+//   var isDown = true;
+//   var isLeft = true;
 
-  var lowLimit = 3;
-  var highLimit = 6;
+//   var lowLimit = 3;
+//   var highLimit = 6;
 
-  // Adjust speed of H and V (for random feeling)
-  var randHDuration = Math.floor(Math.random() * (highLimit * 6500)) + (lowLimit + 6500);
-  var randVDuration = Math.floor(Math.random() * (highLimit * 6500)) + (lowLimit + 6500);
+//   // Adjust speed of H and V (for random feeling)
+//   var randHDuration = Math.floor(Math.random() * (highLimit * 6500)) + (lowLimit + 6500);
+//   var randVDuration = Math.floor(Math.random() * (highLimit * 6500)) + (lowLimit + 6500);
 
-  function hAnimate() {
-    if (isLeft) {
-      $("#object").animate(
-        {
-          left: $(".bounce-wrapper").innerWidth() - $("#object").outerWidth(),
-        },
-        {
-          duration: randHDuration,
-          easing: "linear",
-          queue: false,
-          complete: function () {
-            isLeft = false;
-            hAnimate();
-          },
-        }
-      );
-    } else {
-      $("#object").animate(
-        {
-          left: 0,
-        },
-        {
-          duration: randHDuration,
-          easing: "linear",
-          queue: false,
-          complete: function () {
-            isLeft = true;
-            hAnimate();
-          },
-        }
-      );
-    }
-  }
+//   function hAnimate() {
+//     if (isLeft) {
+//       $("#object").animate(
+//         {
+//           left: $(".bounce-wrapper").innerWidth() - $("#object").outerWidth(),
+//         },
+//         {
+//           duration: randHDuration,
+//           easing: "linear",
+//           queue: false,
+//           complete: function () {
+//             isLeft = false;
+//             hAnimate();
+//           },
+//         }
+//       );
+//     } else {
+//       $("#object").animate(
+//         {
+//           left: 0,
+//         },
+//         {
+//           duration: randHDuration,
+//           easing: "linear",
+//           queue: false,
+//           complete: function () {
+//             isLeft = true;
+//             hAnimate();
+//           },
+//         }
+//       );
+//     }
+//   }
 
-  function vAnimate() {
-    if (isDown) {
-      $("#object").animate(
-        {
-          top: $(".bounce-wrapper").innerHeight() - $("#object").outerHeight(),
-        },
-        {
-          duration: randVDuration,
-          easing: "linear",
-          queue: false,
-          complete: function () {
-            isDown = false;
-            vAnimate();
-          },
-        }
-      );
-    } else {
-      $("#object").animate(
-        {
-          top: 0,
-        },
-        {
-          duration: randVDuration,
-          easing: "linear",
-          queue: false,
-          complete: function () {
-            isDown = true;
-            vAnimate();
-          },
-        }
-      );
-    }
-  }
+//   function vAnimate() {
+//     if (isDown) {
+//       $("#object").animate(
+//         {
+//           top: $(".bounce-wrapper").innerHeight() - $("#object").outerHeight(),
+//         },
+//         {
+//           duration: randVDuration,
+//           easing: "linear",
+//           queue: false,
+//           complete: function () {
+//             isDown = false;
+//             vAnimate();
+//           },
+//         }
+//       );
+//     } else {
+//       $("#object").animate(
+//         {
+//           top: 0,
+//         },
+//         {
+//           duration: randVDuration,
+//           easing: "linear",
+//           queue: false,
+//           complete: function () {
+//             isDown = true;
+//             vAnimate();
+//           },
+//         }
+//       );
+//     }
+//   }
 
-  hAnimate();
-  vAnimate();
-});
+//   hAnimate();
+//   vAnimate();
+// });
 
 
 
